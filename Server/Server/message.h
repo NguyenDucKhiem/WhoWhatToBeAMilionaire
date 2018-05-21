bad10ae9217761c12392bbeee3abf2c2e4d673d6
NguyenDucKhiem <31624398+NguyenDucKhiem@users.noreply.github.com>
2018-05-21 17:58:41 +0700

    viết  hàm sử lí thông điệp waitting, sửa hàm trong message

--- Server/Server/message.h
+++ Server/Server/message.h
@@ -1,41 +1,144 @@
 #pragma once
 #include "constant.h"
+#include "enum.h"
 
 typedef struct Messages {
-	char messtype[MSG_LENGTH_TYPE];
-	char messrec[MSG_LENGTH_RESULT];
+	char messType[MSG_LENGTH_TYPE];
+	char messResult[MSG_LENGTH_RESULT];
 	char data[MSG_LENGTH_DATA];
 } Messages;
 
-int TypeMessage(Messages mess) {
-	if (strcmp(mess.messtype, MSG_LOGIN))
-		return MSG_TYPE_LOGIN;
-	if (strcmp(mess.messtype, MSG_BEGIN_GAME))
-		return MSG_TYPE_BEGIN_GAME;
-	if (strcmp(mess.messtype, MSG_LOGOUT))
-		return MSG_TYPE_LOGOUT;
-	if (strcmp(mess.messtype, MSG_QUESTION))
-		return MSG_TYPE_QUESTION;
-	if (strcmp(mess.messtype, MSG_CHOOSE_PLAYER))
-		return MSG_TYPE_CHOOSE_PLAYER;
-	if (strcmp(mess.messtype, MSG_ANSWER))
-		return MSG_TYPE_ANSWER;
-	if (strcmp(mess.messtype, MSG_HELP))
-		return MSG_TYPE_HELP;
-	if (strcmp(mess.messtype, MSG_ERROR))
-		return MSG_TYPE_ERROR;
-	if (strcmp(mess.messtype, MSG_END_GAME))
-		return MSG_TYPE_END_GAME;
-	if (strcmp(mess.messtype, MSG_END_CONNECT_SERVER))
-		return MSG_TYPE_END_CONNECT_SERVER;
-	if (strcmp(mess.messtype, MSG_END_CONNECT_CLIENT))
-		return MSG_TYPE_END_CONNECT_CLIENT;
-	if (strcmp(mess.messtype, MSG_NEWS))
-		return MSG_TYPE_NEWS;
-	return MSG_TYPE_ERROR;
+TYPE_MSG TypeMessage(Messages *mess) {
+	if (strcmp(mess->messType, MSG_CLIENT_REQUEST_LOGIN))
+		return CLIENT_REQUEST_LOGIN;
+	if (strcmp(mess->messType, MSG_SERVER_REPLY_LOGIN))
+		return SERVER_REPLY_LOGIN;
+	if (strcmp(mess->messType, MSG_CLIENT_REQUEST_PLAY))
+		return CLIENT_REQUEST_PLAY;
+	if (strcmp(mess->messType, MSG_SERVER_REPLY_PLAY))
+		return SERVER_REPLY_PLAY;
+	if (strcmp(mess->messType, MSG_CLIENT_REQUEST_LOGOUT))
+		return CLIENT_REQUEST_LOGOUT;
+	if (strcmp(mess->messType, MSG_SERVER_REPLY_LOGOUT))
+		return SERVER_REPLY_LOGOUT;
+	if (strcmp(mess->messType, MSG_SERVER_SEND_QUESTION_BEGIN))
+		return  SERVER_SEND_QUESTION_BEGIN;
+	if (strcmp(mess->messType, MSG_CLIENT_ANSWER_QUESTION_BEGIN))
+		return CLIENT_ANSWER_QUESTION_BEGIN;
+	if (strcmp(mess->messType, MSG_SERVER_ANSWER_QUESTION_BEGIN))
+		return SERVER_ANSWER_QUESTION_BEGIN;
+	if (strcmp(mess->messType, MSG_SERVER_SEND_QUESTION))
+		return SERVER_SEND_QUESTION;
+	if (strcmp(mess->messType, MSG_CLIENT_ANSWER_QUESTION))
+		return CLIENT_ANSWER_QUESTION;
+	if (strcmp(mess->messType, MSG_CLIENT_USE_HELP_50_50))
+		return CLIENT_USE_HELP_50_50;
+	if (strcmp(mess->messType, MSG_SERVER_ANSWER_HELP_50_50))
+		return SERVER_ANSWER_HELP_50_50;
+	if (strcmp(mess->messType, MSG_CLIENT_USE_HELP_CALL))
+		return CLIENT_USE_HELP_CALL;
+	if (strcmp(mess->messType, MSG_SERVER_ANSWER_HELP_CALL))
+		return SERVER_ANSWER_HELP_CALL;
+	if (strcmp(mess->messType, MSG_CLIENT_USE_HELP_ASK_AUDIENCE))
+		return CLIENT_USE_HELP_ASK_AUDIENCE;
+	if (strcmp(mess->messType, MSG_SERVER_ANSWER_HELP_AUDIENCE))
+		return SERVER_ANSWER_HELP_AUDIENCE;
+	if (strcmp(mess->messType, MSG_CLIENT_USE_HELP_ADVISORY))
+		return CLIENT_USE_HELP_ADVISORY;
+	if (strcmp(mess->messType, MSG_SERVER_ANSWER_HELP_ADVISORY))
+		return SERVER_ANSWER_HELP_ADVISORY;
+	if (strcmp(mess->messType, MSG_SERVER_NEWS))
+		return SERVER_NEWS;
+	if (strcmp(mess->messType, MSG_CLIENT_WANT_END_GAME))
+		return CLIENT_WANT_END_GAME;
+	if (strcmp(mess->messType, MSG_SERVER_ACCEPT_END_GAME))
+		return SERVER_ACCEPT_END_GAME;
+	if (strcmp(mess->messType, MSG_CLIENT_WANT_QUIT))
+		return CLIENT_WANT_QUIT;
+	if (strcmp(mess->messType, MSG_SERVER_ERROR))
+		return SERVER_ERROR;
+	if (strcmp(mess->messType, MSG_CLIENT_ERROR))
+		return CLIENT_ERROR;
+	return MESSAGE_ERROR;
 }
 
-void SetMessage(Messages *mess, const char messtype[], char data[]) {
-	strcpy(mess->messtype, messtype);
-	strcmp(mess->data, data);
+/*
+char *StringMessageType(TYPE_MSG type) {
+	switch (type)
+	{
+	case CLIENT_REQUEST_LOGIN:
+		return MSG_CLIENT_REQUEST_LOGIN;
+	case SERVER_REPLY_LOGIN:
+		return MSG_SERVER_REPLY_LOGIN;
+	case CLIENT_REQUEST_PLAY:
+		return MSG_CLIENT_REQUEST_PLAY;
+	case SERVER_REPLY_PLAY:
+		return MSG_SERVER_REPLY_PLAY;
+	case CLIENT_REQUEST_LOGOUT:
+		return MSG_CLIENT_REQUEST_LOGOUT;
+	case SERVER_REPLY_LOGOUT:
+		return MSG_SERVER_REPLY_LOGOUT;
+	case SERVER_SEND_QUESTION_BEGIN:
+		return MSG_SERVER_SEND_QUESTION_BEGIN;
+	case CLIENT_ANSWER_QUESTION_BEGIN:
+		return MSG_CLIENT_ANSWER_QUESTION_BEGIN;
+	case SERVER_ANSWER_QUESTION_BEGIN:
+		return MSG_SERVER_ANSWER_QUESTION_BEGIN;
+	case SERVER_SEND_QUESTION:
+		return MSG_SERVER_SEND_QUESTION;
+	case CLIENT_ANSWER_QUESTION:
+		return MSG_CLIENT_ANSWER_QUESTION;
+	case SERVER_ANSWER_QUESTION:
+		return MSG_SERVER_ANSWER_QUESTION;
+	case CLIENT_USE_HELP_50_50:
+		return MSG_CLIENT_USE_HELP_50_50;
+	case SERVER_ANSWER_HELP_50_50:
+		return MSG_SERVER_ANSWER_HELP_50_50;
+	case CLIENT_USE_HELP_CALL:
+		return MSG_CLIENT_USE_HELP_CALL;
+	case SERVER_ANSWER_HELP_CALL:
+		return MSG_SERVER_ANSWER_HELP_CALL;
+	case CLIENT_USE_HELP_ASK_AUDIENCE:
+		return MSG_CLIENT_USE_HELP_ASK_AUDIENCE;
+	case SERVER_ANSWER_HELP_AUDIENCE:
+		return MSG_SERVER_ANSWER_HELP_AUDIENCE;
+	case CLIENT_USE_HELP_ADVISORY:
+		return MSG_CLIENT_USE_HELP_ADVISORY;
+	case SERVER_ANSWER_HELP_ADVISORY:
+		return MSG_SERVER_ANSWER_HELP_ADVISORY;
+	case SERVER_NEWS:
+		return MSG_SERVER_NEWS;
+	case CLIENT_WANT_END_GAME:
+		return MSG_CLIENT_WANT_END_GAME;
+	case SERVER_ACCEPT_END_GAME:
+		return MSG_SERVER_ACCEPT_END_GAME;
+	case SERVER_ERROR:
+		return MSG_SERVER_ERROR;
+	case CLIENT_ERROR:
+		return MSG_CLIENT_ERROR;
+	default:
+		return NULL;
+	}
+}
+*/
+
+
+int ResultMess(Messages *mess) {
+	int opcode = mess->messResult[1] - '0';
+	if (opcode > 9 || opcode < 0) return -10;
+	switch (mess->messResult[0])
+	{
+	case '+':
+		return opcode;
+	case '-':
+		return -opcode;
+	default:
+		return -10;			//error
+	}
+}
+
+void SetMessage(Messages *mess,const char messtype[],const char messResult[], char data[]) {
+	strcpy(mess->messType, messtype);
+	strcpy(mess->messResult, messResult);
+	strcpy(mess->data, data);
 }
--- Server/Server/thread.h
+++ Server/Server/thread.h
@@ -1,15 +1,12 @@
 ﻿#pragma once
 
-#include "message.h"
-#include "player.h"
-#include "mylibraly.h"
+
+#include "waitting.h"
 
 //
-Players *playersWaitting[MAX_CLIENT_WAITTING];
-WSAEVENT eventsWaitting[MAX_CLIENT_WAITTING];
 
-Players *playersPlaying[MAX_CLIENT_PLAYING];
-WSAEVENT eventsPlaying[MAX_CLIENT_PLAYING];
+
+
 
 //Stuct contain information of each thread
 //có 2 biến 1 biến ThreadWaitting ThreadPlaying
@@ -83,41 +80,47 @@ unsigned __stdcall WaittingThread(LPVOID parma) {
 					//Define index of  client in the section array
 					idxSect = i + idx + pThInfo->indexOfThread * WSA_MAXIMUM_WAIT_EVENTS;
 
-					WSAGetOverlappedResult(playersWaitting[idxSect]->sock, &sections[idxSect]->overlap, &transByte, FALSE, &flags);
+					WSAGetOverlappedResult(playersWaitting[idxSect]->client, &playersWaitting[idxSect]->overlap, &transByte, FALSE, &flags);
 
 					if (transByte == 0) {
 						//
-						closeSectionEx(i + idx, pThInfo);
+						DeletePlayerInThread(pThInfo, i + idx);
 						continue;
 					}
 					else {
 						//Tranfer success
-						if (sections[idxSect]->operation == RECV) {
+						if (playersWaitting[idxSect]->operation == RECV) {
 							//Process msg from client
-							sections[idxSect]->buff[transByte] = '\0';
-							processRecvMsg(idxSect, sections[idxSect]->buff, BUFF_SIZE, &sections[idxSect]->sendByte);
+							playersWaitting[idxSect]->buff[transByte] = '\0';
+
+							//action watting
+							//processRecvMsg(idxSect, playersWaitting[idxSect]->buff, BUFF_SIZE, &playersWaitting[idxSect]->sendByte);
+
 							//Send msg to client, usse WSASend()
-							sections[idxSect]->operation = SEND;
-							ResetEvent(events[idxSect]);
-							if (WSASend(sections[idxSect]->sock, &sections[idxSect]->dataBuff, 1, &sections[idxSect]->sendByte,
-								flags, &sections[idxSect]->overlap, NULL) == SOCKET_ERROR) {
+							playersWaitting[idxSect]->operation = SEND;
+							ResetEvent(eventsWaitting[idxSect]);
+							if (WSASend(playersWaitting[idxSect]->client, &playersWaitting[idxSect]->dataBuff, 1, &playersWaitting[idxSect]->sendByte,
+								flags, &playersWaitting[idxSect]->overlap, NULL) == SOCKET_ERROR) {
 								if (WSAGetLastError() != WSA_IO_PENDING) {
 									//Hve error in transfer
-									closeSectionEx(i + idx, pThInfo);
+									DeletePlayerInThread(pThInfo, i + idx);
 									continue;
 								}
 							}
+
+							//
 						}
 						else {
 							//Send 1 byte msg success request to receive msg from client
-							resetSection(idxSect);
+							// reset thì các giá trị về 0 -> WSA lỗi ??
+							//resetSection(idxSect);
 							flags = 0;
 							//Post Recv request
-							if (WSARecv(sections[idxSect]->sock, &sections[idxSect]->dataBuff, 1, &transByte, &flags, &sections[idxSect]->overlap, NULL) == SOCKET_ERROR) {
+							if (WSARecv(playersWaitting[idxSect]->client, &playersWaitting[idxSect]->dataBuff, 1, &transByte, &flags, &playersWaitting[idxSect]->overlap, NULL) == SOCKET_ERROR) {
 								if (WSAGetLastError() != WSA_IO_PENDING) {
 									//Have error in the socket index
 									printf("WSARecv() failed with error %d\n", WSAGetLastError());
-									closeSectionEx(i + idx, pThInfo);
+									DeletePlayerInThread(pThInfo, i + idx);
 								}
 							}
 						}
